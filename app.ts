efa355a2d6febac94f6021ea3e0dd2aa01a13629
DesmondMM <mutsakaradesmond@gmail.com>
2016-02-17 09:41:28 +0200

    Created a ProductRow class and template

--- app.ts
+++ app.ts
@@ -37,7 +37,24 @@ class Product {
   outputs:['product'],
   host: {'class': 'item'},
   directives:[ProductImage, ProductDepartment, PriceDisplay],
+  template: `
+  <product-image [product]="product"></product-image>
+  <div claass="content">
+    <div class="header">{{product.name}}</div>
+    <div class="meta">
+      <div class="product-sku">SKU #{{product.sku}}</div>
+    </div>
+    <div class="description">
+      <product-department [product]="product"></product-department>
+    </div>
+  </div>
+  <price-display [price]="product.price"></price-display>
+  `
 })
+
+class ProductRow{
+  product: Product;
+}
 
 /**
  * @ProductList: A componet for rendering all ProductRows and
